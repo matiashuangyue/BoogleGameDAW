cef19cdcea3f8349651c92c8b54beea8de6acec4
Juan <juancruzrodriguez3@gmail.com>
2024-08-01 14:41:16 -0300

    Archivo JavaScript para cambiar tema agregado

---
+++ script/Theme.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            themeToggleButton.textContent = 'Modo Claro';
+        }
+    }
+
+    themeToggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+
+        let theme = 'light-mode';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+            themeToggleButton.textContent = 'Modo Claro';
+        } else {
+            themeToggleButton.textContent = 'Modo Oscuro';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
